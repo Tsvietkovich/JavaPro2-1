9a9315c3db2cc6e58b52c77cb90c71de7562f5dc
Mariya Tsvietkovich <tsvetkovich@gmail.com>
2017-10-05 15:35:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         try {
         String path = "trains.xml";
-        String pathUpdate = "test.xml";
+        
 
-        TrainsUpdater updater = new TrainsUpdater(path,pathUpdate);
+        TrainsUpdater updater = new TrainsUpdater(path);
         updater.addTrain("3","Odessa","Vinnitsa","19.10.2017","20:00");
         updater.createXMLDocument();
 
-        System.out.println(new TrainsSercher(pathUpdate).getTrainsByDeparture("15:00", "19:00"));
+        System.out.println(new TrainsSercher(path).getTrainsByDeparture("15:00", "19:00"));
 
         } catch (TransformerException e) {
             e.printStackTrace();
